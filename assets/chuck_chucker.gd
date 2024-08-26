cb94ad64e8fee12b77e2828fa0dc870141cc985d
LittleCarlito <59811183+LittleCarlito@users.noreply.github.com>
2024-08-26 14:49:04 -0500

    code cleanup

--- assets/chuck_chucker.gd
+++ assets/chuck_chucker.gd
@@ -9,48 +9,28 @@ class_name ChuckChucker
 @onready var cameraController: Node3D = $CameraController
 @onready var frontDetection: ShapeCast3D = $FrontDetect
 var stopwatch: StopWatch = StopWatch.new()
+# TODO Draw bezier curve between launch container and aiming node using control nodes
 var aimingNode: Node3D = Node3D.new()
-
-
-# TODO Add comments to handle methods that are missing them
-# TODO See about adding self. to internal property and method references
+var aimingControl: Node3D = Node3D.new()
+var launchControl: Node3D = Node3D.new()
 
 func _ready() -> void: 
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
-	# Handle jump logic
-	_handle_jump(delta)
-	# Camera controls
-	_handle_camera_controls()	
-	# Check for player action
-	_handle_action(delta)
-	# Check for player interaction
-	_handle_interact()
-	# Check for character movement
-	_handle_movement()
+	self._handle_jump(delta)
+	self._handle_camera_controls()
+	self._handle_player_action(delta)
+	self._handle_player_interact()
+	self._handle_movement()
 
 ## Actions when disk is thrown
-func _handle_action(delta: float) -> void:
+func _handle_player_action(delta: float) -> void:
 	if playerDisk.visible:
 		if Input.is_action_pressed(USER_INPUT.ACTION.PRIMARY):
-			stopwatch.isHeld(delta)
-			var heldTime: float = stopwatch.getTime()
-			aimingNode.position = self.global_position
-			aimingNode.position.y = 0
-			aimingNode.basis = diskLauncher.global_basis
-			var translateVector: Vector3 = Vector3(0, 0, -(heldTime * 10))
-			aimingNode.translate(translateVector)
-			DrawUtil.point(aimingNode.position)
+			self._power_up_launch(delta)
 		if Input.is_action_just_released(USER_INPUT.ACTION.PRIMARY):
-			var multiplier = min(stopwatch.getTime(), GLOBAL_SETTINGS.DISK.MAX_HOLD)
-			stopwatch.reset()
-			toggle_equiped(false)
-			var newDisk = thrownDisk.instantiate()
-			get_tree().get_root().add_child(newDisk)
-			newDisk.global_transform = diskLauncher.global_transform
-			newDisk.linear_velocity = -newDisk.global_transform.basis.z * (GLOBAL_SETTINGS.DISK.LAUNCH_SPEED * multiplier)
-			diskContainer.rotation.x = 0
+			self._launch_disk()
 
 ## Actions to be performed when MOVE_JUMP is pressed
 func _handle_jump(delta: float) -> void:
@@ -76,13 +56,13 @@ func _handle_camera_controls() -> void:
 			diskContainer.rotate_x(deg_to_rad(-GLOBAL_SETTINGS.CAMERA.ROTATE_SPEED))
 
 ## Handle player pressing interact button
-func _handle_interact() -> void:
+func _handle_player_interact() -> void:
 	if Input.is_action_just_pressed(USER_INPUT.ACTION.INTERACT) and frontDetection.is_colliding():
 		var collidingCount = frontDetection.get_collision_count()
 		for n in collidingCount:
 			var collidingObject = frontDetection.get_collider(0)
 			if collidingObject != null and collidingObject.name == ASSET_MANAGEMENT.DISK.NAME:
-				toggle_equiped(true)
+				self.toggle_equiped(true)
 				collidingObject.queue_free()
 
 ## Detects and executes movements
@@ -101,6 +81,28 @@ func _handle_movement() -> void:
 	move_and_slide()
 	# Keep camera up
 	cameraController.position = lerp(cameraController.position, position, GLOBAL_SETTINGS.CAMERA.PAN_SPEED)
+
+## Power up launch and create aim line
+func _power_up_launch(delta: float) -> void:
+	stopwatch.isHeld(delta)
+	var heldTime: float = stopwatch.getTime()
+	aimingNode.position = self.global_position
+	aimingNode.position.y = 0
+	aimingNode.basis = self.global_basis
+	var translateVector: Vector3 = Vector3(0, 0, -(heldTime * 10))
+	aimingNode.translate(translateVector)
+	DrawUtil.point(aimingNode.position)
+
+## Launch disk and reset objects
+func _launch_disk() -> void:
+	var multiplier = min(stopwatch.getTime(), GLOBAL_SETTINGS.DISK.MAX_HOLD)
+	stopwatch.reset()
+	self.toggle_equiped(false)
+	var newDisk = thrownDisk.instantiate()
+	get_tree().get_root().add_child(newDisk)
+	newDisk.global_transform = diskLauncher.global_transform
+	newDisk.linear_velocity = -newDisk.global_transform.basis.z * (GLOBAL_SETTINGS.DISK.LAUNCH_SPEED * multiplier)
+	diskContainer.rotation.x = 0
 
 ## Toggles the visibility logic when character has item equiped
 func toggle_equiped(value: bool) -> void:
