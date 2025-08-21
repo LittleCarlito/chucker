413321835e8e1ec3c8e840df104e2b45492ab431
LittleCarlito <littlecarlitogithub@gmail.com>
2025-08-21 16:24:51 -0500

    Repackage characters

--- assets/items/disks/common/throwable_item.gd
+++ assets/items/disks/common/throwable_item.gd
@@ -21,7 +21,6 @@ enum AIM_TYPE {
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
